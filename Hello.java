d82e5604c0d7791a332f81abec05241f5da26689
DII <DavidFredricksonII@gmail.com>
2016-12-22 12:39:54 -0500

    Hello world, First Java program and Git upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+/**
+* Created by David on 10/7/16.... Java Started on 5/23/16 Tripp approves!
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!");
+        System.out.println("Hello David!!");
+
+        System.out.println("myVariables");
+        int myfirstnumber = (10+80)+ (5+5);
+        int mysecoondnumber = 10;
+        int mythirdnumber = myfirstnumber * 50;
+
+        int myTotal = myfirstnumber + mysecoondnumber + mythirdnumber;
+
+        int mylastone = myTotal -1000;
+
+        System.out.println(mylastone);
+
+
+    }
+ }
